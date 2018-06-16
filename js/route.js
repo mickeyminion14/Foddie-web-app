8cc2e005c206a05ac32ce8d4c33385129532b15d
mickeyminion14 <sarthakagrawal6@gmail.com>
2018-06-16 12:59:01 +0530

    first commit

---
+++ js/route.js
@@ -0,0 +1,84 @@
+ var app=angular.module("FirstApp",["ngRoute"]);
+      app.config(function ($routeProvider) {
+        // body...
+        $routeProvider
+          .when("/login",{
+            templateUrl:"./views/login.html",
+            controller :"emp"
+        })
+          .when("/signup",{
+            templateUrl :"./views/signup.html",
+            controller :"emp1"
+        }).when("/dashboard",{
+            templateUrl :"./views/dashboard.html",
+            controller :"emp1"  
+        })  .when("/profile",{
+            templateUrl :"./views/profile.html",
+            controller :"emp1"
+        })
+      });
+
+    app.controller("emp",function ($rootScope,$location) {
+      
+      $rootScope.username="";
+       $rootScope.password="";
+       $rootScope.loc=$location.absUrl();
+
+       $rootScope.validateLogin=function(username,password){
+        if(username=='user1'&&password=="password"){
+         M.toast({html: 'LOGGED IN SUCCESSFULLY !'})
+        $location.path('/profile');
+        }
+       else{
+          M.toast({html: ' PASSWORD OR USERNAME INCORRECT '})
+       
+       $location.path('/404');
+        }
+}
+
+      });
+     app.controller("emp1",function ($rootScope) {
+      
+      $rootScope.first_name;
+      });
+      app.controller("emp2",function ($rootScope) {
+      
+      $rootScope.first_name;
+      });
+ // var app=angular.module("FirstApp",["ui.router"]);
+ //      app.config(function($stateProvider,$urlRouterProvider){
+ //        $urlRouterProvider.when("","/Dashboard");
+
+ //        $stateProvider
+ //          .state('Dashboard',{          // this is not the url we have given but the name of the state
+ //            url: '/Dashboard' ,     //here we are providing a url we want to show the users.
+ //            templateUrl: "./index.html",
+ //            controller: "emp1"
+ //        })
+ //          .state('signup',{
+ //            url: '/signup',
+ //            templateUrl: "./views/signup.html",
+ //            controller: "emp2"
+ //        })
+ //          .state('login',{
+ //            url: '/login',
+ //            templateUrl: "./views/login.html",
+ //            controller: "emp3"
+ //        }) 
+ //          .state('profile',{
+ //            url: '/profile',
+ //            templateUrl: "./views/profile.html",
+ //            controller: "emp3"
+ //        })
+
+ //      });
+ //      app.controller("emp1",function($scope){
+ //        $scope.a="PARENT CONTROLLER";
+
+ //      });
+ //      app.controller("emp2",function($scope){
+ //        $scope.b="PAGE 1 CONTROLLER";
+ //      });
+ //      app.controller("emp3",function($scope){
+ //        $scope.c="PAGE 2 CONTROLLER";
+ //      });
