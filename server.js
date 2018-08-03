641f583a61a22f5b44fa76becf5a4ee6b72a2cdd
mickeyminion14 <sarthakagrawal6@gmail.com>
2018-08-03 12:25:00 +0530

    hello friends, chai pee lo

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var bodyparser = require('body-parser');
 var express = require ('express');
 var app = express();
-// var routes = require('./app/routes')
-// app.use('/', routes);
+var routes = require('./app/routes')
+app.use('/', routes);
 var fs = require('fs');
 var path  = require ('path');
 
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 8200;
 const HOST = process.env.host || '0.0.0.0';
 app.use(bodyparser.urlencoded ({extended : false}));
 
-// app.use(express.static('./public'));
+app.use(express.static('./public'));
 
 app.get('/', (req, res) => {
 	res.send("Hello World");
