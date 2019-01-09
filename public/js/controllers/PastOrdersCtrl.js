851e1037a83a0de634cc490d5d94d12fa477f68a
mickeyminion14 <sarthakagrawal6@gmail.com>
2019-01-09 20:10:51 +0530

    past orders working with pagination

--- public/js/controllers/PastOrdersCtrl.js
+++ public/js/controllers/PastOrdersCtrl.js
@@ -14,7 +14,7 @@ angular.module("PastOrdersCtrl",[]).controller("PastOrdersController", function 
   console.log("past orders ctrl");
   $scope.datarecieved=false;
   $scope.currentindex=0;
-
+  $scope.newindex=0;
   $scope.totalpast=0;
   $scope.getTotal(0);
   // const ordersProcessedArray = {};
@@ -49,8 +49,9 @@ angular.module("PastOrdersCtrl",[]).controller("PastOrdersController", function 
   //  }
 
   //  console.log($rootScope.pastorders);
-  $rootScope.finalpastorders=$rootScope.temp.response[1];
+  $rootScope.finalpastorders=$rootScope.temp.response[0];
   $scope.getTotal(0);
+
   })
   .error(function (data) {
     console.log('Error: ' + data);
@@ -61,6 +62,14 @@ angular.module("PastOrdersCtrl",[]).controller("PastOrdersController", function 
     $scope.currentindex=index;
     console.log("total function called");
     console.log(index);
+    // if(index == 0) {
+    //   $("li").removeClass("active"); 
+    //   $("#0").parent().addClass("active");
+    //   console.log($("#"+0).parent());
+    //    $("#"+0).parent().addClass("active");
+    //   // console.log(document.getElementsByTagName("li")[1]);
+    //   console.log("added");
+    // } 
 
     angular.forEach($rootScope.temp.response[index], function (value, key) {
 
@@ -70,15 +79,19 @@ angular.module("PastOrdersCtrl",[]).controller("PastOrdersController", function 
   };
 
   $scope.onPagination = function (index) {
-    if(index==0) {
+    $("li").removeClass("active"); 
+    console.log(  $("#"+index).parent());
+    $("#"+index).parent().addClass("active");
+    if(index == 0) {
+     
       $("#back").addClass("disabled");
       $("#forward").removeClass("disabled");
     }
-    if(index >0) {
+    if(index > 0) {
       $("#back").removeClass("disabled");
     }
     // console.log($rootScope.temp.response.length);
-    if(index == $rootScope.temp.response.length-1) {
+    if(index == $rootScope.temp.response.length - 1) {
       $("#forward").addClass("disabled");
       $("#back").removeClass("disabled");
     }
@@ -86,6 +99,7 @@ angular.module("PastOrdersCtrl",[]).controller("PastOrdersController", function 
     if(index < $rootScope.temp.response.length-1) {
       $("#forward").removeClass("disabled");
     }
+    
     $scope.currentindex=index;
     $scope.totalpast=0;
     console.log(index);
@@ -95,12 +109,57 @@ angular.module("PastOrdersCtrl",[]).controller("PastOrdersController", function 
   };
 
   $scope.onPaginationBack = function () {
-    if($scope.currentindex==0) {
+    $("li").removeClass("active"); 
 
+    
+
+    $scope.newindex=$scope.currentindex-1;
+    if($scope.newindex < 0 ){
+      $scope.newindex=0;
     }
+    if($scope.newindex==0) {
+      $("#back").addClass("disabled");
+      $("#forward").removeClass("disabled");
+    }
+
+    if($scope.newindex > 0) {
+      $("#back").removeClass("disabled");
+      $("#forward").removeClass("disabled");
+    }
+    $("#"+$scope.newindex).parent().addClass("active");
+    $scope.totalpast=0;
+    console.log($scope.newindex);
+    $rootScope.finalpastorders=$rootScope.temp.response[$scope.newindex];
+    $scope.getTotal($scope.newindex);
+    $scope.currentindex=$scope.newindex;
+
+    console.log($scope.currentindex);
   }
 
   $scope.onPaginationForward = function () {
+    $("li").removeClass("active"); 
+    $scope.newindex=$scope.currentindex+1;
+    if($scope.newindex >  $rootScope.temp.response.length - 1 ){
+      $scope.newindex= $rootScope.temp.response.length - 1;
+      $("#forward").addClass("disabled");
+      $("#back").removeClass("disabled");
+    }
+    if($scope.newindex== $rootScope.temp.response.length - 1) {
+      $("#forward").addClass("disabled");
+      $("#back").removeClass("disabled");
+    }
 
+    if($scope.newindex <  $rootScope.temp.response.length - 1) {
+      $("#forward").removeClass("disabled");
+      $("#back").removeClass("disabled");
+    }
+    $("#"+$scope.newindex).parent().addClass("active");
+    $scope.totalpast=0;
+    console.log($scope.newindex);
+    $rootScope.finalpastorders=$rootScope.temp.response[$scope.newindex];
+    $scope.getTotal($scope.newindex);
+    $scope.currentindex=$scope.newindex;
+
+    console.log($scope.currentindex);
   }
 });
